dcc3fca47a248265061e6c7bc418ca63d0ac63cc
˜victor˜ <guhcostan@gmail.com>
2019-09-06 14:20:14 -0300

    ANTLR linguagem alguma implementado

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,21 @@
-public class Main {
-    public static void main() {
+import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.Token;
 
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+    public static void main(String...args) throws IOException {
+
+
+        String filePath = "/Users/aluno/IdeaProjects/compiladores/src/teste.alguma";
+        ANTLRFileStream input = new ANTLRFileStream(filePath);
+        Scanner scanner = new Scanner(input);
+        List<? extends Token> tokens = scanner.getAllTokens();
+
+        for (Token token : tokens) {
+            System.out.printf("{%s}\n", token.getText());
+        }
     }
 }
